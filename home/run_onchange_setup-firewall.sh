777ebe8dcd1e0912527ddd44a9460ead76bf574c
mihakrumpestar <70652456+mihakrumpestar@users.noreply.github.com>
2024-05-21 18:14:33 +0200

    U

--- home/run_onchange_setup-firewall.sh
+++ home/run_onchange_setup-firewall.sh
@@ -3,8 +3,8 @@
 echo -e "\n\e[1m Firewall config \e[0m\n"
 
 sudo ufw allow 22/tcp # SSH
-sudo ufw allow 1714:1764/udp # KDE connect
 sudo ufw allow 1714:1764/tcp # KDE connect
+sudo ufw allow 1714:1764/udp # KDE connect
 
 sudo ufw enable
 sudo ufw reload
