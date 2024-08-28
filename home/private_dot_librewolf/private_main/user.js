faf1eea915e0faa83c560532cbc950e52de8075d
mihakrumpestar <70652456+mihakrumpestar@users.noreply.github.com>
2024-08-28 23:58:29 +0200

    U

--- home/private_dot_librewolf/private_main/user.js
+++ home/private_dot_librewolf/private_main/user.js
@@ -10,3 +10,13 @@ user_pref("browser.theme.toolbar-theme", 0); // Same as above
 user_pref("dom.events.asyncClipboard.clipboardItem", true);
 
 user_pref("network.protocol-handler.external.mailto", false); // Remove annoying "add application for mailto links"
+
+// Privacy
+user_pref("privacy.resistFingerprinting", true); // Instead of using CanvasBlocker extension
+user_pref("privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts", true);
+user_pref("webgl.disabled", true);
+user_pref("media.peerconnection.enabled", false); // Disabling WebRTC
+user_pref("geo.enabled", false); // Disables geolocation tracking
+user_pref("privacy.firstparty.isolate", true); // Isolates cookies to the first party domain
+
+user_pref("general.useragent.override", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.3");
